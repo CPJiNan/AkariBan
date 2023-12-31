ee53ce6323b971b58ff990615dc7b4d0221b076f
CPJiNan <2759278070@qq.com>
2023-12-31 11:19:01 +0800

    [+] 修复若干问题

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ taboolib {
     install("platform-bukkit")
     install("expansion-command-helper")
     classifier = null
-    version = "6.0.12-35"
+    version = "6.0.12-56"
     description {
         contributors {
             name("CPJiNan")
