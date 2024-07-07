ef0d7cb980fffa9f45e87f6b33938d23d8746903
季楠 <2759278070@qq.com>
2024-07-07 21:50:51 +0800

    [3.1.5] [+]升级Taboolib版本至6.1.2-test1 [+]新增永久封禁功能

--- src/main/kotlin/com/github/cpjinan/plugin/akariban/internal/command/subCommand/Ban.kt
+++ src/main/kotlin/com/github/cpjinan/plugin/akariban/internal/command/subCommand/Ban.kt
@@ -13,10 +13,37 @@ import taboolib.expansion.createHelper
 import taboolib.module.lang.sendLang
 import java.time.LocalDateTime
 
+@Suppress("DuplicatedCode")
 object Ban {
     val ban = subCommand {
         createHelper()
-        dynamic("player", optional = false).dynamic("duration", optional = false) {
+        dynamic("player", optional = false) {
+            execute<ProxyCommandSender> { sender: ProxyCommandSender, context: CommandContext<ProxyCommandSender>, _: String ->
+                val playerName = context["player"]
+                val banReason = ConfigManager.getDefaultBanReason()
+                val banDuration = ConfigManager.getTimeFormat().getString("Permanent")!!
+                val banTime = LocalDateTime.now().formatToString(FormatManager.getTimeFormat())
+                val banningAdmin = sender.name
+                if (playerName.isPlayerOnline()) {
+                    AkariBanAPI.banOnlinePlayer(
+                        Bukkit.getPlayer(playerName)!!,
+                        banReason,
+                        banDuration,
+                        banTime,
+                        banningAdmin
+                    )
+                } else {
+                    AkariBanAPI.banOfflinePlayer(
+                        playerName,
+                        banReason,
+                        banDuration,
+                        banTime,
+                        banningAdmin
+                    )
+                }
+                sender.sendLang("Ban-Success", playerName)
+            }
+        }.dynamic("duration", optional = true) {
             execute<ProxyCommandSender> { sender: ProxyCommandSender, context: CommandContext<ProxyCommandSender>, _: String ->
                 val playerName = context["player"]
                 val banReason = ConfigManager.getDefaultBanReason()
